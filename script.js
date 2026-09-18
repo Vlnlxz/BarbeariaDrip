f3c476c11ec111a296073cff940c243506f031d9
Vlnlxz <vinicius.rodrigo.neves@escola.pr.gov.br>
2026-09-18 22:02:00 +0000

    Pronto

---
+++ script.js
@@ -0,0 +1,12 @@
+const burgerBtn = document.getElementById('burgerBtn');
+  const mobileMenu = document.getElementById('mobileMenu');
+  burgerBtn.addEventListener('click', () => {
+    const isOpen = mobileMenu.classList.toggle('open');
+    burgerBtn.setAttribute('aria-expanded', isOpen);
+  });
+  mobileMenu.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => {
+      mobileMenu.classList.remove('open');
+      burgerBtn.setAttribute('aria-expanded', 'false');
+    });
+  });
